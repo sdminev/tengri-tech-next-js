c1ebba020d61b4f6951b74081c7b1a6ae345dbd7
Stefan Minev <stefanminev@gmail.com>
2025-03-13 19:19:56 +0200

    Change favicon

--- src/app/resources/content.js
+++ src/app/resources/content.js
@@ -88,7 +88,7 @@ const about = {
   },
   work: {
     display: true, // set to false to hide this section
-    title: "Work Experience",
+    title: "Our Expertise",
     experiences: [
       {
         company: "FLY",
